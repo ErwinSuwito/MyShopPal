c0dbb19c43af2c847fa57e073c1f5806f52202a7
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-04-01 16:12:21 +0700

    Updated to v10

--- app/src/main/java/com/myshoppal/ui/activities/BaseActivity.kt
+++ app/src/main/java/com/myshoppal/ui/activities/BaseActivity.kt
@@ -1,11 +1,15 @@
 package com.myshoppal.ui.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.myshoppal.R
+import kotlinx.android.synthetic.main.dialog_progress.*
 
 open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var mProgressDialog: Dialog
 
     fun showErrorSnackBar(message: String, errorMessage: Boolean)
     {
@@ -22,5 +26,22 @@ open class BaseActivity : AppCompatActivity() {
         }
 
         snackbar.show()
+    }
+
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(this)
+
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+
+        mProgressDialog.tv_progress_text.text = text
+
+        mProgressDialog.setCancelable(false)
+        mProgressDialog.setCanceledOnTouchOutside(false)
+
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        mProgressDialog.dismiss()
     }
 }
--- app/src/main/java/com/myshoppal/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/myshoppal/ui/activities/RegisterActivity.kt
@@ -87,12 +87,18 @@ class RegisterActivity : BaseActivity() {
     private fun registerUser() {
         if (validateRegisterDetails())
         {
+
+            showProgressDialog(resources.getString(R.string.please_wait))
+
             val email: String = et_email.text.toString().trim { it <= ' '}
             val password: String = et_email.text.toString().trim { it <= ' '}
 
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(
                             OnCompleteListener<AuthResult> { task ->
+
+                                hideProgressDialog()
+
                                 if (task.isSuccessful)
                                 {
                                     val firebaseUser: FirebaseUser = task.result!!.user!!

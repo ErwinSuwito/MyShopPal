058449e64a039ff1371996cb85d121feb1688266
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-04-01 16:22:43 +0700

    Updated to v11

--- app/src/main/java/com/myshoppal/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/myshoppal/ui/activities/LoginActivity.kt
@@ -3,12 +3,15 @@ package com.myshoppal.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import com.myshoppal.R
 import com.myshoppal.utils.MSPTextViewBold
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -20,10 +23,50 @@ class LoginActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        val tv_register: MSPTextViewBold = findViewById(R.id.tv_register)
-        tv_register.setOnClickListener {
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(intent)
+        // Assign click listener to the views
+        // Click event assigned to Forgot Password text.
+        tv_forgot_password.setOnClickListener(this)
+        // Click event assigned to Login button.
+        btn_login.setOnClickListener(this)
+        // Click event assigned to Register text.
+        tv_register.setOnClickListener(this)
+    }
+
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null)
+        {
+            when (v.id)
+            {
+                R.id.tv_forgot_password -> {
+
+                }
+
+                R.id.btn_login -> {
+                    validateLoginDetails()
+                }
+
+                R.id.tv_register -> {
+                    val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/myshoppal/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/myshoppal/ui/activities/RegisterActivity.kt
@@ -104,6 +104,10 @@ class RegisterActivity : BaseActivity() {
                                     val firebaseUser: FirebaseUser = task.result!!.user!!
 
                                     showErrorSnackBar("You are registered successfully. Your user id is ${firebaseUser.uid}", false)
+
+                                    // Signs out the user and return to the login screen
+                                    FirebaseAuth.getInstance().signOut()
+                                    finish()
                                 }
                                 else
                                 {

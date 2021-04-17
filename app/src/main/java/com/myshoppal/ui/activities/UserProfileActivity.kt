6200d132f86d13741cd791dc372cf2ada859e9c0
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-04-17 16:47:14 +0700

    Updated to v25

--- app/src/main/java/com/myshoppal/ui/activities/UserProfileActivity.kt
+++ app/src/main/java/com/myshoppal/ui/activities/UserProfileActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -43,7 +44,7 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         et_email.setText(userDetails.email)
 
         iv_user_photo.setOnClickListener(this@UserProfileActivity)
-
+        btn_save.setOnClickListener(this@UserProfileActivity)
     }
 
     override fun onClick(v: View?) {
@@ -68,6 +69,12 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
                                 arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                                 Constants.READ_STORAGE_PERMISSION_CODE
                         )
+                    }
+                }
+
+                R.id.btn_save -> {
+                    if (validateUserProfileDetails()) {
+                        showErrorSnackBar("Your details are valid. You can update them.",false)
                     }
                 }
             }
@@ -115,6 +122,23 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         } else if (resultCode == Activity.RESULT_CANCELED) {
             // A log is printed when user close or cancel the image selection.
             Log.e("Request Cancelled", "Image selection cancelled")
+        }
+    }
+
+    private fun validateUserProfileDetails(): Boolean {
+        return when {
+            // We have kept the user profile picture is optional.
+            // The FirstName, LastName, and Email Id are not editable when they come from the login screen.
+            // The Radio button for Gender always has the default selected value.
+
+            // Check if the mobile number is not empty as it is mandatory to enter.
+            TextUtils.isEmpty(et_mobile_number.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_mobile_number), true)
+                false
+            }
+            else -> {
+                true
+            }
         }
     }
 }
